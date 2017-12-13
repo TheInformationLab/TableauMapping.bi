2938c485f7cc058bcb6418e7dd723d06619ab230
Craig Bloodworth <craig.bloodworth@theinformationlab.co.uk>
2017-12-13 15:30:08 +0000

    Fixed placement of sign in form

--- assets/app/auth/auth.component.ts
+++ assets/app/auth/auth.component.ts
@@ -4,9 +4,7 @@ import { AuthService } from './auth.service';
 @Component({
   selector: 'app-auth',
   template: `
-  <div class="container" style="margin-top:100px;">
-    <router-outlet></router-outlet>
-  </div>
+      <router-outlet></router-outlet>
   `
 })
 
