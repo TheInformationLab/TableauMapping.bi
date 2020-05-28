c1ae38c18337a0c8e5ea229e858246d272060209
Craig Bloodworth <craig.bloodworth@theinformationlab.co.uk>
2020-05-28 23:22:21 +0100

    Switch to project level HOST env variable

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function haltOnTimedout(req, res, next){
 
 var options = { useNewUrlParser: true, useUnifiedTopology: true };
 
-var dbhost = 'tableaumapping-yi1n8.mongodb.net/tableaumappingmapbox?retryWrites=true&w=majority' || process.env.HOST || 'localhost:27017/tableau-mapping2';
+var dbhost = process.env.HOST || 'localhost:27017/tableau-mapping2';
 var dbuser = process.env.DBUSER || null;
 var dbpass = process.env.DBPASS || null;
 var dburi = 'mongodb+srv://';
