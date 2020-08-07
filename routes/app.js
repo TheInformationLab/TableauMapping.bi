e91cf932c6e9c1ffe53d49bf080c955fb0a9daae
Craig Bloodworth <craig.bloodworth@theinformationlab.co.uk>
2020-08-07 15:47:08 +0100

    Embed routes into express routing

--- routes/app.js
+++ routes/app.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+router.get("/wdc*", function (req, res, next) {
+    res.redirect("https://wdc.tableaumapping.bi");
+    next
+});
+
+router.get("/index/*", function (req, res, next) {
+    res.redirect("https://index.tableaumapping.bi");
+    next
+});
+
+router.post("/index/*", function (req, res, next) {
+    res.redirect("https://index.tableaumapping.bi");
+    next
+});
+
+router.delete("/index/*", function (req, res, next) {
+    res.redirect("https://index.tableaumapping.bi");
+    next
+});
+
 router.get('/', function (req, res, next) {
     console.log(req.headers['user-agent']);
     res.render('index');
